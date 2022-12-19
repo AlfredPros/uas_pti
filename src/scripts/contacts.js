14c5d071684929e6c1f419b70c35a57a6308185c
Mikazuki17 <115234588+Mikazuki17@users.noreply.github.com>
2022-12-20 00:34:41 +0700

    about us

---
+++ src/scripts/contacts.js
@@ -0,0 +1,31 @@
+const contacts = [
+    {
+      name: "Alexander Michael Nugroho",
+      imgURL: "",
+      nim: "00000045127",
+      email: "@student.ac.id"
+    },
+    {
+      name: "Alfred Kuhlman",
+      imgURL:
+        "",
+      nim: "00000047678",
+      email: "@student.ac.id"
+    },
+    {
+      name: "Gregorius Lai",
+      imgURL:
+        "",
+      nim: "00000045410",
+      email: "@student.ac.id"
+    },
+    {
+      name: "Loysing Ryono Ismanto",
+      imgURL:
+        "",
+      nim: "00000052026",
+      email: "@student.ac.id"
+    }
+  ];
+  export default contacts;
+  
